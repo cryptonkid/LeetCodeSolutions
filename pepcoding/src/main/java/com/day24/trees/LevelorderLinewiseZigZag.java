b067537d9a6b5f9e7486659e752a7a0278baf5eb
cryptonkid <cryptonkid@gmail.com>
2022-04-02 17:38:39 +0530

    GenericTree : Important : ZigZag Traversal

---
+++ pepcoding/src/main/java/com/day24/trees/LevelorderLinewiseZigZag.java
@@ -0,0 +1,157 @@
+package com.day24.trees;
+
+
+    import java.io.*;
+import java.util.*;
+
+    public class LevelorderLinewiseZigZag {
+        private static class Node {
+            int data;
+            ArrayList<Node> children = new ArrayList<>();
+        }
+
+        public static void display(Node node) {
+            String str = node.data + " -> ";
+            for (Node child : node.children) {
+                str += child.data + ", ";
+            }
+            str += ".";
+            System.out.println(str);
+
+            for (Node child : node.children) {
+                display(child);
+            }
+        }
+
+        public static Node construct(int[] arr) {
+            Node root = null;
+
+            Stack<Node> st = new Stack<>();
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == -1) {
+                    st.pop();
+                } else {
+                    Node t = new Node();
+                    t.data = arr[i];
+
+                    if (st.size() > 0) {
+                        st.peek().children.add(t);
+                    } else {
+                        root = t;
+                    }
+
+                    st.push(t);
+                }
+            }
+
+            return root;
+        }
+
+        public static int size(Node node) {
+            int s = 0;
+
+            for (Node child : node.children) {
+                s += size(child);
+            }
+            s += 1;
+
+            return s;
+        }
+
+        public static int max(Node node) {
+            int m = Integer.MIN_VALUE;
+
+            for (Node child : node.children) {
+                int cm = max(child);
+                m = Math.max(m, cm);
+            }
+            m = Math.max(m, node.data);
+
+            return m;
+        }
+
+        public static int height(Node node) {
+            int h = -1;
+
+            for (Node child : node.children) {
+                int ch = height(child);
+                h = Math.max(h, ch);
+            }
+            h += 1;
+
+            return h;
+        }
+
+        public static void traversals(Node node){
+            System.out.println("Node Pre " + node.data);
+
+            for(Node child: node.children){
+                System.out.println("Edge Pre " + node.data + "--" + child.data);
+                traversals(child);
+                System.out.println("Edge Post " + node.data + "--" + child.data);
+            }
+
+            System.out.println("Node Post " + node.data);
+        }
+
+        /**
+         * Using Stack and Queue
+         * @param node
+         */
+        public static void levelOrderLinewiseZZ(Node node){
+            Queue<Node> que = new ArrayDeque<>();
+            Stack<Node> st = new Stack<>();
+
+            que.add(node);
+
+            int level =1;
+
+            while(que.size() >0)
+            {
+                int size = que.size();
+
+                while(size-- >0)
+                {
+                    if(level % 2 == 1) //odd level
+                    {
+                        Node rem = que.remove();
+
+                        System.out.print(rem.data + " ");
+
+                        for (Node child : rem.children) {
+                            que.add(child);// queue is used for traversal
+                            st.push(child);
+                        }
+                    }else{
+                        //Even level
+                        Node strem = st.pop();
+                        Node querem = que.remove();
+
+                        System.out.print(strem.data+" ");
+
+                        for(Node child : querem.children)
+                            que.add(child);//for queue
+                    }
+
+                }
+                System.out.println();
+                level++;
+
+            }
+
+        }
+
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] values = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(values[i]);
+            }
+
+            Node root = construct(arr);
+            levelOrderLinewiseZZ(root);
+        }
+
+    }
