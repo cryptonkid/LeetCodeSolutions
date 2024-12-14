f2a8e7c06e6170218f4f0d78ae371fbc747df9aa
cryptonkid <cryptonkid@gmail.com>
2024-12-14 06:35:50 +0100

    BST: ValidBST and its all approaches.

--- leetCodeSolutions/src/com/leetcode/solutions/bst/ValidBST.java
+++ leetCodeSolutions/src/com/leetcode/solutions/bst/ValidBST.java
@@ -24,4 +24,31 @@ public class ValidBST {
         return leftIsValid && rightIsValid;
     }
     
+    //Solution II
+    public boolean isValidBST2(TreeNode root) {
+    	return validBSTHelper(root, null, null);        
+    }
+    
+    private boolean validBSTHelper(TreeNode node, TreeNode leftTree, TreeNode rightTree ){
+        if(node== null) return true;
+        
+        if( leftTree != null && node.val <= leftTree.val){
+            return false;
+        }
+        if( rightTree != null && node.val >= rightTree.val){
+            return false;
+        }
+        
+        boolean leftIsValid = validBSTHelper(node.left, leftTree, node);
+        boolean rightIsValid = validBSTHelper(node.right, node, rightTree );
+        
+        return leftIsValid && rightIsValid;
+    }
+    
+    /*
+     * Solution-III
+     * Create a list in Inorder and then check if the elements are sorted.
+     */
+    
+    
 }
